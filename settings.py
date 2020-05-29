97ac7d42bc91037ce921ff44335e01a9b7743c89
thealeksandrsheykin <alexander_sheykin@mail.ru>
2020-05-29 21:48:47 +0300

    Adding movement

--- settings.py
+++ settings.py
@@ -8,6 +8,9 @@ class Settings():
     def __init__(self):
         '''Initialize the game's settings.'''
         #Screen settings
-        self.screen_width = 1200
-        self.screen_height = 800
+        self.screen_width = 800
+        self.screen_height = 400
         self.bg_color = (230,230,230)
+
+        #Ship settings
+        self.ship_speed_factor = 1.5
